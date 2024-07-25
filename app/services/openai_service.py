1ec3c0c9ba3b3a47917a1a5da2f9548bc7a996a8
Tigist Woldearegai <tigistlina@gmail.com>
2024-07-25 17:52:12 -0500

    adds routes to create lessons and retrieve lesson from firestore. uses openai API and connects firestore to the project

---
+++ app/services/openai_service.py
@@ -0,0 +1,14 @@
+from openai import OpenAI
+
+client = OpenAI()
+
+def generate_openai_lesson(prompt):
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": prompt}
+        ],
+        max_tokens=150
+    )
+    lesson_text = response.choices[0].message.content.split("\n")
+    return lesson_text
