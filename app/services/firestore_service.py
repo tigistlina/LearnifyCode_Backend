e2b2920aed0f0d65d60a347bff5c06e79f37119d
Tigist Woldearegai <tigistlina@gmail.com>
2024-07-29 19:50:05 -0500

    adds moreee routes

--- app/services/firestore_service.py
+++ app/services/firestore_service.py
@@ -15,3 +15,30 @@ def fetch_lessons(db):
     lessons_list = [{"id": lesson.id, "data": lesson.to_dict()}
                     for lesson in lessons]
     return lessons_list
+
+
+def fetch_lesson_by_id(db, lesson_id):
+    lesson_ref = db.collection('lessons').document(lesson_id)
+    lesson = lesson_ref.get()
+    if lesson.exists:
+        return {"id": lesson.id, "data": lesson.to_dict()}
+    return None
+
+
+def search_lessons_by_title(db, title_query):
+    # Use a case-insensitive search if you need to
+    lessons = db.collection('lessons').stream()
+
+    # Filter lessons that contain the title_query in their prompt
+    filtered_lessons = [
+        {
+            "id": lesson.id,
+            "prompt": lesson.to_dict().get('prompt', ''),
+            "lesson": lesson.to_dict().get('lesson', '')
+        }
+        for lesson in lessons
+        if title_query.lower() in lesson.to_dict().get('prompt', '').lower()
+    ]
+
+    return filtered_lessons
+
