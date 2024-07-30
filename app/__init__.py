15144ce52873bc7d64dc9c2c8cec823578541be8
Tigist Woldearegai <tigistlina@gmail.com>
2024-07-30 14:59:16 -0500

    modifies __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,20 +2,22 @@ from flask import Flask
 from firebase_admin import credentials, firestore, initialize_app, _apps
 import os
 
+
 def create_app(testing=False):
     app = Flask(__name__)
 
     # Conditionally use Firestore emulator settings
     if testing:
         os.environ['FIRESTORE_EMULATOR_HOST'] = 'localhost:8080'
-        os.environ['FIRESTORE_PROJECT_ID'] = 'learnify code'
+        os.environ['FIRESTORE_PROJECT_ID'] = 'learnifycode'
         app.config['TESTING'] = True
 
     # Get the path to the Firebase credentials JSON file from environment variables
     firebase_credentials_path = os.getenv('FIREBASE_CREDENTIALS_PATH')
 
     if not firebase_credentials_path:
-        raise ValueError("The path to the Firebase credentials JSON file cannot be accessed.")
+        raise ValueError(
+            "The path to the Firebase credentials JSON file cannot be accessed.")
 
     # Initialize Firebase Admin SDK only if not already initialized
     if not _apps:
