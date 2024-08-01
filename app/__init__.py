864dcc1d4814d262176216d4d746920f5de5728b
Tigist Woldearegai <tigistlina@gmail.com>
2024-08-01 04:51:12 -0500

    set up Dockerfile with necessary config and added .dockerignore file

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,27 @@
 from flask import Flask
 from firebase_admin import credentials, firestore, initialize_app, _apps
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def create_app(testing=False):
     app = Flask(__name__)
 
-    # Conditionally use Firestore emulator settings
-    if testing:
-        os.environ['FIRESTORE_EMULATOR_HOST'] = 'localhost:8080'
-        os.environ['FIRESTORE_PROJECT_ID'] = 'learnifycode'
+    # Get Firestore project ID from environment variable
+    firestore_project_id = os.getenv('FIRESTORE_PROJECT_ID')
+    firestore_emulator_host = os.getenv('FIRESTORE_EMULATOR_HOST')
+
+    # Conditionally use Firestore emulator settings based on environment variable
+    use_emulator = os.getenv('USE_FIRESTORE_EMULATOR', 'false').lower() == 'true'
+
+    if testing or use_emulator:
+        os.environ['FIRESTORE_EMULATOR_HOST'] = firestore_emulator_host
+        os.environ['FIRESTORE_PROJECT_ID'] = firestore_project_id
         app.config['TESTING'] = True
+    else:
+        os.environ.pop('FIRESTORE_EMULATOR_HOST', None)
+        os.environ.pop('FIRESTORE_PROJECT_ID', None)
 
     # Get the path to the Firebase credentials JSON file from environment variables
     firebase_credentials_path = os.getenv('FIREBASE_CREDENTIALS_PATH')
@@ -22,7 +33,7 @@ def create_app(testing=False):
     # Initialize Firebase Admin SDK only if not already initialized
     if not _apps:
         cred = credentials.Certificate(firebase_credentials_path)
-        initialize_app(cred)
+        initialize_app(cred, {'projectId': firestore_project_id})
 
     app.db = firestore.client()
 
